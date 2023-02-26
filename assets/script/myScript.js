b19176e7518ab663e3e78d7af36fbafab2eddd08
Abdul Haseeb <haseeb7063952@gmail.com>
2023-02-26 19:39:58 +0000

    update

--- assets/script/myScript.js
+++ assets/script/myScript.js
@@ -3,3 +3,19 @@
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+var textElem = document.getElementById("text");
+var textArray = [
+  "I'm Abdul Haseeb.",
+  "I'm Front End Web Developer.",
+  "I'm a Freelancer.",
+];
+var currentText = 0;
+
+function changeText() {
+  textElem.innerHTML = textArray[currentText];
+  currentText = (currentText + 1) % textArray.length;
+}
+
+changeText();
+setInterval(changeText, 1000);
