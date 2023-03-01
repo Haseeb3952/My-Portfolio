282a5c4820e0095eb15b407e8b4165714c2796be
Abdul Haseeb <haseeb7063952@gmail.com>
2023-03-01 20:14:35 +0000

    update

--- assets/script/myScript.js
+++ assets/script/myScript.js
@@ -20,6 +20,24 @@ function changeText() {
 changeText();
 setInterval(changeText, 1000);
 
+// Get the button
+var btnTop = document.getElementById("btn-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+btnTop.addEventListener("click", function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
 // Number Container
 
 function generateNumbers(start, end) {
