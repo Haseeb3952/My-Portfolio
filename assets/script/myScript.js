12d965e026c111b591999fa653f5ae17de7a08c2
Abdul Haseeb <haseeb7063952@gmail.com>
2023-03-02 15:01:25 +0000

    update

--- assets/script/myScript.js
+++ assets/script/myScript.js
@@ -20,24 +20,27 @@ function changeText() {
 changeText();
 setInterval(changeText, 1000);
 
-// Get the button
-var btnTop = document.getElementById("btn-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btnTop.style.display = "block";
-  } else {
-    btnTop.style.display = "none";
-  }
-};
-
 // When the user clicks on the button, scroll to the top of the document
 btnTop.addEventListener("click", function () {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
+// Go to Top Button Hide on Top of the Page
+
+const goTopBtn = document.getElementById("go-top-btn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    goTopBtn.style.display = "block";
+  } else {
+    goTopBtn.style.display = "none";
+  }
+});
+
+goTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 // Number Container
 
 function generateNumbers(start, end) {
